2e986246e00f9fc45d5caecce24578e707461dcc
Shyamprasad Narapareddy <49093331+n-shyamprasad@users.noreply.github.com>
2023-11-29 23:13:49 +1300

    Add files via upload

---
+++ OriginalPost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PostDTOConsole
+{
+    // Your original class corresponding to the JSON structure
+    public class OriginalPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        // Add other properties as needed
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.Json;
+using AutoMapper;
+
+namespace PostDTOConsole
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            // Your JSON data as a string
+            string jsonData = "{\"Id\": 1, \"Title\": \"Sample Title\", \"Content\": \"Sample Content\"}";
+
+            // Deserialize JSON to your original class
+            var originalPost = JsonSerializer.Deserialize<OriginalPost>(jsonData);
+
+            // Configure AutoMapper
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<OriginalPost, PostDTO>();
+                // Add additional mappings if needed
+            });
+
+            // Create the mapper
+            var mapper = new Mapper(config);
+
+            // Map the original post to PostDTO
+            var postDto = mapper.Map<PostDTO>(originalPost);
+
+            // Print the result
+            Console.WriteLine($"Mapped PostDTO: Id = {postDto.Id}, Title = {postDto.Title}, Content = {postDto.Content}");
+            Console.Read();
+        }
+    }
+}
